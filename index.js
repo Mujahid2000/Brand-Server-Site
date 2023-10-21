955f672ab25d763a57d94400f8c657a79acbc29d
Mujahid2000 <developermujahid2001@gmail.com>
2023-10-22 01:32:32 +0600

    add update texte

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
             res.send(result);
         })
 
-        // update
+        //  update form
 
         app.get('/update/:id', async (req, res) => {
             const id = req.params.id;
